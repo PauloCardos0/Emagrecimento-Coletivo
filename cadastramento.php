06e605777ae8d2aef705715ab09858bd4298e377
Paulo Cardoso <pouvini5@gmail.com>
2021-10-12 21:38:56 -0300

    Enviando pro banco de dados

---
+++ cadastramento.php
@@ -0,0 +1,39 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$matricula= $_POST['matricula'];
+$altura = $_POST['altura'];
+$peso = $_POST['peso'];
+$sexo = $_POST['sexo'];
+
+
+//Conectanco ao Banco de dados
+$hostname = "localhost";
+$bancodedados = "emagrecimento_coletivo";
+$usuario = "root";
+$senha = "";
+
+$conn = mysqli_connect($hostname, $usuario, $senha, $bancodedados );
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo"-------------------------------------------------"."<br>";
+echo "Conectado com sucesso". "<br>";
+echo"-------------------------------------------------"."<br>";
+
+
+$sql = "INSERT INTO Cadastramento (nome, email, matricula, peso, altura, sexo) VALUES ('$nome', '$email', '$matricula', '$peso', '$altura', '$sexo')";
+if (mysqli_query($conn, $sql)) {
+      echo"-------------------------------------------------"."<br>";
+      echo "<strong>OS DADOS FORAM REGISTRADOS NO SEU BANCO DE DADOS</strong>"."<br>";
+      echo"-------------------------------------------------";
+
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+
+
+?>
