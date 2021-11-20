247c72c4c7e58c2bd96f7b061fec218ff5400bde
Pablo Santos <pablo.roldao.santos@gmail.com>
2021-11-20 20:37:17 -0300

    atualizando cadastro, login e index

---
+++ antigo/cadastramento.php
@@ -0,0 +1,36 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$matricula= $_POST['matricula'];
+$altura = $_POST['altura'];
+$peso = $_POST['peso'];
+$sexo = $_POST['sexo'];
+
+
+//Conectando ao BD
+$hostname = "localhost";
+$bancodedados = "emagrecimento_coletivo";
+$usuario = "root";
+$senha = "";
+
+$conn = mysqli_connect($hostname, $usuario, $senha, $bancodedados );
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$sql = "INSERT INTO Cadastramento (nome, email, matricula, peso, altura, sexo) VALUES ('$nome', '$email', '$matricula', '$peso', '$altura', '$sexo')";
+if (mysqli_query($conn, $sql)) {
+      echo"-------------------------------------------------"."<br>";
+      echo "<strong>Os dados foram cadastrados</strong>"."<br>";
+      echo"-------------------------------------------------";
+
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+header ("location: index.html");
+
+?>
